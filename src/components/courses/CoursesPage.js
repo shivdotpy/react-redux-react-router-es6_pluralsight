aeadd29cedae5002afdceff4c9f7178d200cc95a
shiv <shivssharma1990@gmail.com>
2019-09-19 11:11:00 +0530

    redux implementation done

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,40 +5,20 @@ import PropTypes from "prop-types";
 import { bindActionCreators } from "redux";
 
 class CoursesPage extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      course: {
-        title: ""
-      }
-    };
+  componentDidMount() {
+    this.props.actions.loadCourses().catch(error => {
+      alert("loading courses failed: " + error);
+    });
   }
-
-  handleChange = event => {
-    const course = { ...this.state.course, title: event.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = event => {
-    event.preventDefault();
-    this.props.actions.createCourse(this.state.course);
-  };
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <React.Fragment>
         <h2>Courses</h2>
-        <h3>Add Course</h3>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        />
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </React.Fragment>
     );
   }
 }
